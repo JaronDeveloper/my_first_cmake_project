626a3723d9ab0443df809b8711f9df3f230262ca
Prodject <Liter.Hari@gmail.com>
2023-10-25 10:32:37 +0200

    first commit

---
+++ my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,21 @@
+﻿// my_first_cmake_project.cpp: определяет точку входа для приложения.
+//
+
+#include "my_first_cmake_project.h"
+#include <iostream>
+#include <Windows.h> 
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string Name;	
+	cout << "Введите имя: ";
+	cin >> Name;
+	cout << "Здравствуйте, " << Name << "!" << endl;
+	return 0;
+}
